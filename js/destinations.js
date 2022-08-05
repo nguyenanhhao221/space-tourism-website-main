53ff266ffcb48c26a226b9a0565741f4479ef89a
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-08-06 03:14:51 +0700

    feat: :sparkles: crew page - allow list to change the content of the pages by adding javascript

--- js/destinations.js
+++ js/destinations.js
@@ -1,3 +1,4 @@
+//Data is an array contain objects depends on the url
 import { data } from './helpers.js';
 
 const tabButtons = document.querySelectorAll('.list');
@@ -5,7 +6,7 @@ const tabButtons = document.querySelectorAll('.list');
 tabButtons.forEach(button => {
   button.addEventListener('click', () => {
     const destinationData = data.find(
-      element => element.name.toLowerCase() === button.id
+      element => element.name.toLowerCase() === button.id.toLowerCase()
     );
     Object.entries(destinationData).forEach(element => {
       if (element[0] !== 'images') {
@@ -14,7 +15,8 @@ tabButtons.forEach(button => {
       if (element[0] === 'images') {
         document.querySelector('picture source').srcset = element[1].webp;
         document.querySelector('picture img').src = element[1].png;
-        document.querySelector('picture img').alt = `The ${button.id}`;
+
+        document.querySelector('picture img').alt = button.id;
       }
       return;
     });
