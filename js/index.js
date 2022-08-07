daf549d7d917f269a29ed4aa3a18d9b930d76cef
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-08-07 19:34:27 +0700

    refactor: :recycle: deleted unused html file and refactor destinations.js into index.js also

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-import { fetchData, getPathName } from './helpers.js';
-
 // Implement Hamburger Menu
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const nav = document.querySelector('.primary-navigation');
@@ -35,5 +33,48 @@ const addActiveState = (buttonArr, currentButton) => {
 list.forEach(button => {
   button.addEventListener('click', () => {
     addActiveState(list, button);
+  });
+});
+
+//Data is an array contain objects depends on the url
+import { data } from './helpers.js';
+
+const tabButtons = document.querySelectorAll('.list');
+
+tabButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const destinationData = data.find(
+      element => element.name.toLowerCase() === button.id.toLowerCase()
+    );
+    Object.entries(destinationData).forEach(element => {
+      if (element[0] !== 'images') {
+        document.querySelector(`#${element[0]}`).innerHTML = element[1];
+      }
+      if (element[0] === 'images') {
+        // for the technology pages where the images contains 2 version of landscape and portrait
+        if (element[1].portrait && element[1].landscape) {
+          console.log(
+            document.querySelector('picture source.technology-image-mobile')
+          );
+          document.querySelector(
+            'picture source.technology-image-mobile'
+          ).srcset = element[1].landscape;
+          document.querySelector('picture img.technology-image-mobile').src =
+            element[1].landscape;
+          document.querySelector(
+            'picture source.technology-image-desktop'
+          ).srcset = element[1].portrait;
+          document.querySelector('picture img.technology-image-desktop').src =
+            element[1].portrait;
+          document.querySelector('picture img').alt = button.id;
+          return;
+        }
+        document.querySelector('picture source').srcset = element[1].webp;
+        document.querySelector('picture img').src = element[1].png;
+
+        document.querySelector('picture img').alt = button.id;
+      }
+      return;
+    });
   });
 });
